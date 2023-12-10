d528ce1c6df51a3977d9687cd2abcb9690a55b90
猪猪侠 <zhumingli@hotmail.com>
2023-12-10 23:26:17 +0800

    Update d11soft.sh

--- d11soft.sh
+++ d11soft.sh
@@ -492,9 +492,9 @@ server {
     location / {
         proxy_pass http://$target_ip;
         proxy_set_header Host $custom_host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
     }
 }
 EOF"
